7447ad033896d85d96500642a340ae206db29739
PaleoCrafter <PaleoCrafter@users.noreply.github.com>
2020-11-08 18:26:37 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.20a3188570699c8d7fba60825c9f6a50.js
@@ -0,0 +1,658 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac5883e850f4740661e8e94915c8f91",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "d1b440bd5752ec7c825b",
+    "url": "/css/app.ea70d372.css"
+  },
+  {
+    "revision": "62240645dda52f4b90da42affa544a98",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "bc10bf7cbeea7694aa85a07e7e119f5b",
+    "url": "/img/characters/Adolin_-_VIII._Strength_.jpg"
+  },
+  {
+    "revision": "a991fc9c412399c5b9254484f246c24a",
+    "url": "/img/characters/Cultivation_Eduard_Coll.jpg"
+  },
+  {
+    "revision": "b92c56b000c168fd4847183bb7382cd2",
+    "url": "/img/characters/Cultivation_and_the_Nightwatcher_2.jpg"
+  },
+  {
+    "revision": "70f92b9770d8468f0e5a0ee86ae29322",
+    "url": "/img/characters/Dalinar_-_IV._The_Emperor_.jpg"
+  },
+  {
+    "revision": "d8d0313e1f3d6ed8b73700f421cb8d27",
+    "url": "/img/characters/Elhokar_-_V._Hierophant.jpg"
+  },
+  {
+    "revision": "ca28da5afacb6cbebfeb642b206e9e57",
+    "url": "/img/characters/Eshonai_-_XIII._Death.jpg"
+  },
+  {
+    "revision": "d7c40585888dcad557c588fef62a697c",
+    "url": "/img/characters/Honor_Kyle_Pearson.jpg"
+  },
+  {
+    "revision": "56328b850d98a04bfe37efad4bebb39e",
+    "url": "/img/characters/Jasnah_-_II._The_High_Priestess_.jpg"
+  },
+  {
+    "revision": "b95cabdc74e4a588261673c1955dd079",
+    "url": "/img/characters/Kaladin_-_XI_Justice_.jpg"
+  },
+  {
+    "revision": "9797d8769e9c3dd717c19114582f562a",
+    "url": "/img/characters/King_Gavilar.jpg"
+  },
+  {
+    "revision": "8e58651e42868d8619922738b2c4306e",
+    "url": "/img/characters/Lift_-_VII._The_Chariot_.jpg"
+  },
+  {
+    "revision": "8550c9a820e60730749c27ddb44542cc",
+    "url": "/img/characters/Moash_-_IX._Hermit.jpg"
+  },
+  {
+    "revision": "2181376d50ffe799ad36f45436d6ab9c",
+    "url": "/img/characters/Navani_-_III._The_Empress.png"
+  },
+  {
+    "revision": "98cbe3414dd6fa96269d222434dfd786",
+    "url": "/img/characters/Odium_-_XV_the_Devil.jpg"
+  },
+  {
+    "revision": "04a25797654b1807198c0e31da552263",
+    "url": "/img/characters/Renarin_-_XVIII._The_Moon_.jpg"
+  },
+  {
+    "revision": "3e3b08c4a0ba298540a7e946398877e3",
+    "url": "/img/characters/Rysn_&_Larkin_by_botanicaxu.jpg"
+  },
+  {
+    "revision": "71da48341cb0cea6d479f96337d6ec0b",
+    "url": "/img/characters/Shallan_-_I._The_Magician_.jpg"
+  },
+  {
+    "revision": "3185aacdef33cb0086b8d2f5cc26921c",
+    "url": "/img/characters/Szeth_-_XX._Judgement_.jpg"
+  },
+  {
+    "revision": "a7516e112c5556e231cd845070640841",
+    "url": "/img/characters/Tanavast.jpg"
+  },
+  {
+    "revision": "3b5183e9d45eff8da4a0f366ecf0eb44",
+    "url": "/img/characters/Taravangian_as_the_Wheel_of_Fortune_Orientation_1.jpg"
+  },
+  {
+    "revision": "0eec2b0c2f7b894f953db0c581b104dc",
+    "url": "/img/characters/Venli_-_XVI._The_Tower_.jpg"
+  },
+  {
+    "revision": "18a61770ad88cbda487d001605d56f08",
+    "url": "/img/characters/Wit-Fool_by_taratjah.jpg"
+  },
+  {
+    "revision": "e51492299e16cabe67ab122089809a3e",
+    "url": "/img/events/Act_of_Mercy_by_Xabi_Gaztelua.jpg"
+  },
+  {
+    "revision": "a5c1f35e358a91c9979d66506ddb2112",
+    "url": "/img/events/Adolin_&_Shallan_-_VI._the_Lovers_.png"
+  },
+  {
+    "revision": "bc10bf7cbeea7694aa85a07e7e119f5b",
+    "url": "/img/events/Adolin_-_VIII._Strength_.jpg"
+  },
+  {
+    "revision": "55ead1248ad2c7973b1e8301eb1086cf",
+    "url": "/img/events/Aesudan_and_Ashertmarn.jpg"
+  },
+  {
+    "revision": "c05389528bee5f4f6e5e2ddfb21e3e2c",
+    "url": "/img/events/Alethkar_Color.jpg"
+  },
+  {
+    "revision": "67c0f5b4de67abfcba307e30524eb755",
+    "url": "/img/events/Amaram_by_Marie_Seeberger.jpg"
+  },
+  {
+    "revision": "198affcf5283e9407e5d06f2c1e5d831",
+    "url": "/img/events/Assassin_in_White_by_Krystian_Biskup.jpg"
+  },
+  {
+    "revision": "1b70d39a2afc758f295008903d6dd998",
+    "url": "/img/events/Azure.jpg"
+  },
+  {
+    "revision": "f82593a588b084fe6306279a6c9d4547",
+    "url": "/img/events/Battle_of_Narak.jpeg"
+  },
+  {
+    "revision": "2e3d23a49083f552cc9aaf9a0a041256",
+    "url": "/img/events/Broken_Oath_by_Antti_Hakosaari.jpg"
+  },
+  {
+    "revision": "a0d0fcda194c27178a37bb5cb10698f3",
+    "url": "/img/events/Call_to_Adventure_-_Willshaper.png"
+  },
+  {
+    "revision": "7ed22608ea138e0c222edf419a6ac35a",
+    "url": "/img/events/Chasmfiend_Fight_by_Ellie_L..png"
+  },
+  {
+    "revision": "4b7ab75d7dc60831166931bc1192c74a",
+    "url": "/img/events/Child_by_Xabi_Gazte.jpg"
+  },
+  {
+    "revision": "f2a65c653a6b3f5c23438b6c5ea7dde1",
+    "url": "/img/events/Choose_a_Side.jpg"
+  },
+  {
+    "revision": "b92c56b000c168fd4847183bb7382cd2",
+    "url": "/img/events/Cultivation_and_the_Nightwatcher_2.jpg"
+  },
+  {
+    "revision": "70f92b9770d8468f0e5a0ee86ae29322",
+    "url": "/img/events/Dalinar_-_IV._The_Emperor_.jpg"
+  },
+  {
+    "revision": "6df6c1f44410399bac2378c066596234",
+    "url": "/img/events/Dalinar_Ambush_LGD.png"
+  },
+  {
+    "revision": "606b42223db2f1375d5ed74af8f1f1b2",
+    "url": "/img/events/Dalinar_and_Navani_by_Marie_Seeberger.jpg"
+  },
+  {
+    "revision": "41a8241c91f1ae9c7b14b6deb4d99b25",
+    "url": "/img/events/Dalinar_the_Blackthorn.jpg"
+  },
+  {
+    "revision": "cf0873bc0d859aaef4eeeea1a5d0d1f4",
+    "url": "/img/events/Darkeyes_by_Gal_Or.jpg"
+  },
+  {
+    "revision": "7ee793ef93ad9ff6025084591c6089d9",
+    "url": "/img/events/Driven_by_Guilt_by_Ari_Ibarra.jpg"
+  },
+  {
+    "revision": "3a318fed79fbac9790c1692f1f9d2885",
+    "url": "/img/events/Duelist_by_Randy_Vargas.jpg"
+  },
+  {
+    "revision": "d8d0313e1f3d6ed8b73700f421cb8d27",
+    "url": "/img/events/Elhokar_-_V._Hierophant.jpg"
+  },
+  {
+    "revision": "d2114e51dc33e9b9c69a0355f6e73915",
+    "url": "/img/events/Eshonai_by_Casey_Fallon.jpg"
+  },
+  {
+    "revision": "cba10d968e0184f3bdf674e8f814da35",
+    "url": "/img/events/Facing_off_with_the_Assassin_in_White.jpg"
+  },
+  {
+    "revision": "20722bf27cac8b5edc3c5dabc29c149d",
+    "url": "/img/events/Fallen_from_Grace_by_Soph_P.jpg"
+  },
+  {
+    "revision": "e943a637d67bb3e58e9a2d757fab8775",
+    "url": "/img/events/First_Ideal_poster.jpg"
+  },
+  {
+    "revision": "94d7038cfb698a376948192ef7b95e45",
+    "url": "/img/events/Gavinor_Shuravf.jpg"
+  },
+  {
+    "revision": "225d1251c7d34acbdead0285f59d99ea",
+    "url": "/img/events/Ghostblood_Agent_by_Soph_P.jpg"
+  },
+  {
+    "revision": "9fe2271b6382cacb0d6bd8026b87e80f",
+    "url": "/img/events/Honor_is_Dead_by_ThomasW.jpg"
+  },
+  {
+    "revision": "be9b80da717e53a30fe940f934e50dd9",
+    "url": "/img/events/Hug_the_Darkness.png"
+  },
+  {
+    "revision": "63df7ea504d5584cce869e0d5620a79f",
+    "url": "/img/events/Inner_Rosharan_System_by_Art_Noob.jpg"
+  },
+  {
+    "revision": "56328b850d98a04bfe37efad4bebb39e",
+    "url": "/img/events/Jasnah_-_II._The_High_Priestess_.jpg"
+  },
+  {
+    "revision": "969037b3b25c6cf96b1ce58980d95c11",
+    "url": "/img/events/Jasnah_by_Audrey_Hotte.png"
+  },
+  {
+    "revision": "481400b7fa8cf38543e8b473331d986d",
+    "url": "/img/events/Jezrien_Oathbringer_Herald.jpg"
+  },
+  {
+    "revision": "b95cabdc74e4a588261673c1955dd079",
+    "url": "/img/events/Kaladin_-_XI_Justice_.jpg"
+  },
+  {
+    "revision": "cb576721a45396162c738e2584a2c45b",
+    "url": "/img/events/Kaladin_young.jpg"
+  },
+  {
+    "revision": "8e58651e42868d8619922738b2c4306e",
+    "url": "/img/events/Lift_-_VII._The_Chariot_.jpg"
+  },
+  {
+    "revision": "51717407f72aef4731fdb4d6a7da93c5",
+    "url": "/img/events/Lighthouse_LittleGreyDragon.jpg"
+  },
+  {
+    "revision": "cf8d0f47d29df3dbca408a3124be1101",
+    "url": "/img/events/Lyn_by_Shuravf.jpg"
+  },
+  {
+    "revision": "f6cfaa4238501a6c137ec41210f63cf8",
+    "url": "/img/events/Midnight_Mother_2.jpg"
+  },
+  {
+    "revision": "b2376988ea42e8d5fa34941edce0d79a",
+    "url": "/img/events/Moash_by_Nick_Vidic.jpg"
+  },
+  {
+    "revision": "2181376d50ffe799ad36f45436d6ab9c",
+    "url": "/img/events/Navani_-_III._The_Empress.png"
+  },
+  {
+    "revision": "54944d685fc0e5bfbfa0abd37c57efde",
+    "url": "/img/events/Oathgate_Locations_(edited).jpg"
+  },
+  {
+    "revision": "628ea1d6092c9770a8cb143a65555a64",
+    "url": "/img/events/Oathgate_by_Artem_Demura.png"
+  },
+  {
+    "revision": "98cbe3414dd6fa96269d222434dfd786",
+    "url": "/img/events/Odium_-_XV_the_Devil.jpg"
+  },
+  {
+    "revision": "480745ec64512dad5eca9e252f6be5b6",
+    "url": "/img/events/Odium_by_Ganna_Pazyniuk.jpg"
+  },
+  {
+    "revision": "dbda8163ce7fa1f5e3e47bd73be4b1a0",
+    "url": "/img/events/Part3_End_Steve_Argyle.jpg"
+  },
+  {
+    "revision": "d39442e994b297f482171dda8f4a5ac2",
+    "url": "/img/events/Plateau_Battle_by_Miguel_Sastre.jpg"
+  },
+  {
+    "revision": "c03ffaae54d617c4abf9245d4f6070d4",
+    "url": "/img/events/Purelake_1.jpeg"
+  },
+  {
+    "revision": "1224618d12f60795a9c94613c920e37f",
+    "url": "/img/events/Purelake_Connor_Chamberlain.jpg"
+  },
+  {
+    "revision": "e51e28eabb2d1ae2121f2990bd84a1bc",
+    "url": "/img/events/Reluctant_Villain_by_Art_Dem.jpg"
+  },
+  {
+    "revision": "04a25797654b1807198c0e31da552263",
+    "url": "/img/events/Renarin_-_XVIII._The_Moon_.jpg"
+  },
+  {
+    "revision": "3bffec07170e47bcbde37d19da655a30",
+    "url": "/img/events/Rysn-and-Larkin.jpg"
+  },
+  {
+    "revision": "9d33c6e5c6904a025dfc87e1b71a1691",
+    "url": "/img/events/Rysn.jpg"
+  },
+  {
+    "revision": "5abcc3e2dd42aee4d178dbca379b340a",
+    "url": "/img/events/Secret_Training_by_Adam_J._Marin.png"
+  },
+  {
+    "revision": "a3d63ac4cfe05fcb3575748b5f66cde5",
+    "url": "/img/events/Seeking_Forgiveness_Jordi_Rapture.jpg"
+  },
+  {
+    "revision": "71da48341cb0cea6d479f96337d6ec0b",
+    "url": "/img/events/Shallan_-_I._The_Magician_.jpg"
+  },
+  {
+    "revision": "086b85743711598889978bc6ad738f57",
+    "url": "/img/events/Shallan_Adolin.jpg"
+  },
+  {
+    "revision": "d2c8e134d6f0c72afe935fe30383ce3d",
+    "url": "/img/events/Shallan_and_Cryptic.jpg"
+  },
+  {
+    "revision": "8520928af3d6ed8102ab53230ad72955",
+    "url": "/img/events/Shallan_and_Jasnah_Reunited.png"
+  },
+  {
+    "revision": "1ac2aa402403014303665b9cd5cc567d",
+    "url": "/img/events/Shallan_and_her_mother.jpg"
+  },
+  {
+    "revision": "188070770e6272e8882fdb6aaa390b2b",
+    "url": "/img/events/Shallan_in_the_Palanaeum.jpg"
+  },
+  {
+    "revision": "ef9f1c6428a8d397cbdb9a91331565fb",
+    "url": "/img/events/Shallan_shardblade1_full.jpg"
+  },
+  {
+    "revision": "c1d97e6c6edb3d6c0ccb8459be2c89fd",
+    "url": "/img/events/Shattering_Connor_Chamberlain.jpg"
+  },
+  {
+    "revision": "2a7f753417f8a364b10657ce38a92f5b",
+    "url": "/img/events/Stormblessed_by_BBQShapez.png"
+  },
+  {
+    "revision": "45f098dcf8f7258fcc719e66a65f85ae",
+    "url": "/img/events/Stormblessed_by_ThomasW.jpg"
+  },
+  {
+    "revision": "58ff4d5657fb918f439b47af2d155eb8",
+    "url": "/img/events/Stubborn_Obstacle_by_Xabi_gazte.jpg"
+  },
+  {
+    "revision": "0a2b6bc07ff19a17f36f09f95a354c2b",
+    "url": "/img/events/Survive_the_Storm_by_Ari_Ibarra.jpg"
+  },
+  {
+    "revision": "3185aacdef33cb0086b8d2f5cc26921c",
+    "url": "/img/events/Szeth_-_XX._Judgement_.jpg"
+  },
+  {
+    "revision": "7db6e25052ac3ff237b6e53372315d28",
+    "url": "/img/events/Szeth_Nale.jpg"
+  },
+  {
+    "revision": "715b79b7e35e2ae3d73cae76cef8738d",
+    "url": "/img/events/Taln_Neirr.png"
+  },
+  {
+    "revision": "3b5183e9d45eff8da4a0f366ecf0eb44",
+    "url": "/img/events/Taravangian_as_the_Wheel_of_Fortune_Orientation_1.jpg"
+  },
+  {
+    "revision": "6223fe8fe4c8d62084114cb5d4ee8ec3",
+    "url": "/img/events/Thaylen_City.jpg"
+  },
+  {
+    "revision": "1fc40fa223d2475d24718911f0a9219e",
+    "url": "/img/events/The_Impossible_Odds_by_Randy_Vargas.jpg"
+  },
+  {
+    "revision": "c3da197b761998727d5831d9f882e144",
+    "url": "/img/events/The_Leap_by_Grant_Hansen.jpg"
+  },
+  {
+    "revision": "6dcb95510ad50e0150cdd527686e49b8",
+    "url": "/img/events/The_Rift_AshfellEverdawn.jpg"
+  },
+  {
+    "revision": "dd353b16b12575767ce6ac08aec65d35",
+    "url": "/img/events/Thepathtohearthstone.png"
+  },
+  {
+    "revision": "f718a81ba00f263ff0f4c13f5156e15c",
+    "url": "/img/events/Thief_by_Ari_Ibarra.jpg"
+  },
+  {
+    "revision": "48d5756f2a01aa770c75384f7008aef5",
+    "url": "/img/events/Thrill.png"
+  },
+  {
+    "revision": "84b57da6e2206ebbc8a503c1d9d9150a",
+    "url": "/img/events/Urithiru3_by_Jacobo_Montoya.jpg"
+  },
+  {
+    "revision": "b275bbc39b03c045f9b46b0e77b2f4c4",
+    "url": "/img/events/Veil_by_LGD.jpg"
+  },
+  {
+    "revision": "0eec2b0c2f7b894f953db0c581b104dc",
+    "url": "/img/events/Venli_-_XVI._The_Tower_.jpg"
+  },
+  {
+    "revision": "97c821ddcf22ce35a3a67fe90486a505",
+    "url": "/img/events/WoR_SH_-_Adolin.jpg"
+  },
+  {
+    "revision": "22d4d9f2e466afe071b2219b0a9e7ec1",
+    "url": "/img/events/diagram.jpg"
+  },
+  {
+    "revision": "76ff4efc02f7f493afb7496f6adad2cd",
+    "url": "/img/events/kaladin.jpg"
+  },
+  {
+    "revision": "1a3874f8a7cff8399b5be62c2315b9ef",
+    "url": "/img/events/the_way_of_kings__excerpt_illustration_by_beagifted.jpg"
+  },
+  {
+    "revision": "1b32168e00b841b294721e4fc50434f0",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "778c1f38ceb95cba5fc0bf0973902732",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "39d97b3bbbdf7e098e4b27e74015b6c2",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "960473f7c1acb1f4e043c44ff1b88f78",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "7ca75e3ea0393d59991212812a81374b",
+    "url": "/img/icons/favicon-114x114.png"
+  },
+  {
+    "revision": "caad3c630dff3da65cc8b9b52217fa40",
+    "url": "/img/icons/favicon-120x120.png"
+  },
+  {
+    "revision": "2fc451c5376bc09441e54d107673a40a",
+    "url": "/img/icons/favicon-144x144.png"
+  },
+  {
+    "revision": "0c44b6fdb1342562aa398b00ffbd484c",
+    "url": "/img/icons/favicon-152x152.png"
+  },
+  {
+    "revision": "c06da8cde36d883a1a4b80f014ea3018",
+    "url": "/img/icons/favicon-160x160.png"
+  },
+  {
+    "revision": "0bf8dec1d5f63bb991ed970f10174bed",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "ae48a177387b27c96f1bdb82e739fa71",
+    "url": "/img/icons/favicon-180x180.png"
+  },
+  {
+    "revision": "9dda1811ddc600c82707744753aec169",
+    "url": "/img/icons/favicon-192x192.png"
+  },
+  {
+    "revision": "adb9f58f8beb2a188e5e1375bfc699fd",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "2fdcfcc531be56792c3706cc5ea49d6a",
+    "url": "/img/icons/favicon-57x57.png"
+  },
+  {
+    "revision": "b4520e683d6299cb2981ce18ad1f123d",
+    "url": "/img/icons/favicon-60x60.png"
+  },
+  {
+    "revision": "52285383ac6b27a0cc769a0b7abbb976",
+    "url": "/img/icons/favicon-64x64.png"
+  },
+  {
+    "revision": "a9310a8e3593af6f377eeea6513e1fb3",
+    "url": "/img/icons/favicon-72x72.png"
+  },
+  {
+    "revision": "6b61b912b1f01dac2e107e7c493bfe60",
+    "url": "/img/icons/favicon-76x76.png"
+  },
+  {
+    "revision": "16468301b1819520257b9fc633eb8b06",
+    "url": "/img/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "dbd443161dd6015370948b133126bd53",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "bb4c5c3cdb317cf462147c7aa38e2f8e",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8ee0932032de2c8573cef220fe9914fd",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "c05389528bee5f4f6e5e2ddfb21e3e2c",
+    "url": "/img/locations/Alethkar_Color.jpg"
+  },
+  {
+    "revision": "b92c56b000c168fd4847183bb7382cd2",
+    "url": "/img/locations/Cultivation_and_the_Nightwatcher_2.jpg"
+  },
+  {
+    "revision": "64aed3545621475873ca948c670652f1",
+    "url": "/img/locations/Frostlands_map.jpeg"
+  },
+  {
+    "revision": "980fdc210b78a7faba2669e1e433b48e",
+    "url": "/img/locations/Honor's_Path_Kyle_Pearson.jpg"
+  },
+  {
+    "revision": "35924b9d57bb0424c49244387fc21aaa",
+    "url": "/img/locations/Kholinar_Illustratio_by_Daitengu_koh.jpg"
+  },
+  {
+    "revision": "64ef03bb226ba225ec7078a0bed3d9ce",
+    "url": "/img/locations/Map_ReshiSea.png"
+  },
+  {
+    "revision": "c03ffaae54d617c4abf9245d4f6070d4",
+    "url": "/img/locations/Purelake_1.jpeg"
+  },
+  {
+    "revision": "ebb93dbb68013dda8346fd5f6a931c7f",
+    "url": "/img/locations/Reshi_Isles_Connor_Chamberlain.jpg"
+  },
+  {
+    "revision": "ddb4f15316479c43214e917720901c2f",
+    "url": "/img/locations/Sea_of_Lost_Lights.jpg"
+  },
+  {
+    "revision": "25443675efeafa0f4d020c4a95575ca4",
+    "url": "/img/locations/Stormseat_Mosaic.jpeg"
+  },
+  {
+    "revision": "6223fe8fe4c8d62084114cb5d4ee8ec3",
+    "url": "/img/locations/Thaylen_City.jpg"
+  },
+  {
+    "revision": "4a9dd5caab03c1ead100ede8b21e0701",
+    "url": "/img/locations/The_Horneater_Peaks_by_Yen_Shu_Liao.jpg"
+  },
+  {
+    "revision": "84b57da6e2206ebbc8a503c1d9d9150a",
+    "url": "/img/locations/Urithiru3_by_Jacobo_Montoya.jpg"
+  },
+  {
+    "revision": "dff9ef3c3537eee700ddc45d94572b4c",
+    "url": "/img/locations/Whelan_Stormlight_Shallan.jpg"
+  },
+  {
+    "revision": "db05fe2360ed29f2505f99e9eebdee86",
+    "url": "/img/locations/Yeddaw.png"
+  },
+  {
+    "revision": "a27a7e7f4633880df3c3fd613fc280d8",
+    "url": "/img/locations/kharbranth.jpg"
+  },
+  {
+    "revision": "d27bc83ae7602bb1637be7e723723cac",
+    "url": "/img/locations/the_rift_by_greatunknown.png"
+  },
+  {
+    "revision": "a8391a1d72370575cdd5256d46b7bf89",
+    "url": "/img/logos/17s.a8391a1d.png"
+  },
+  {
+    "revision": "751954132e5b478c298b6c22eef37e57",
+    "url": "/img/logos/cosmerees.75195413.png"
+  },
+  {
+    "revision": "d42129674568a2ef5c84097339cce884",
+    "url": "/img/logos/roshar.d4212967.png"
+  },
+  {
+    "revision": "22d4d9f2e466afe071b2219b0a9e7ec1",
+    "url": "/img/misc/diagram.jpg"
+  },
+  {
+    "revision": "c987b3edf12ee7ef7bc70ec4372dd3c7",
+    "url": "/img/misc/ghostbloods.png"
+  },
+  {
+    "revision": "39999bee004b2a2a1e798c852b1c93d4",
+    "url": "/img/opengraph.png"
+  },
+  {
+    "revision": "905fc21c8a210ada70161bfb218cd3fa",
+    "url": "/img/opengraph.webp"
+  },
+  {
+    "revision": "fc09aa66fb142cfb52abab8bf1208a14",
+    "url": "/img/paper.fc09aa66.png"
+  },
+  {
+    "revision": "99e29686e9010420161cef48a1db890f",
+    "url": "/img/shattering.99e29686.jpg"
+  },
+  {
+    "revision": "d1b440bd5752ec7c825b",
+    "url": "/js/app.c422f667.js"
+  },
+  {
+    "revision": "4dd202633be9ccfd764e",
+    "url": "/js/chunk-vendors.b3661f2d.js"
+  },
+  {
+    "revision": "d7060cc3944aa18a40b922d8523559e2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
